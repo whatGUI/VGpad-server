9bdd3d3c24813a576b698a0c991a48290092b652
whatGUI <wangzhe5658@163.com>
2022-05-11 01:24:20 +0800

    add LS and RS button support

--- src/vgpad/controller.js
+++ src/vgpad/controller.js
@@ -34,6 +34,14 @@ module.exports = class VGamepad {
     this.controller.button.RIGHT_SHOULDER.setValue(value);
   }
 
+  LEFT_THUMB(value){
+    this.controller.button.LEFT_THUMB.setValue(value);
+  }
+
+  RIGHT_THUMB(value){
+    this.controller.button.RIGHT_THUMB.setValue(value);
+  }
+
   A(value) {
     this.controller.button.A.setValue(value);
   }
